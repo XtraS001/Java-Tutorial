e4deca09865358c37d90e324b98815531468872c
XtraS001 <65765051+XtraS001@users.noreply.github.com>
2020-12-20 00:46:06 +0800

    Update J37_Main_Methods_And_Non_Static_Methods.java

--- Giraffe Java/src/J37_Main_Methods_And_Non_Static_Methods.java
+++ Giraffe Java/src/J37_Main_Methods_And_Non_Static_Methods.java
@@ -1,10 +1,19 @@
 
-public class J37_Main_Methods_And_Non_Static_Methods {
+public class J37_Main_Methods_And_Non_Static_Methods 
+{ //Must change class name to App before running
 	public static void main (String [] args) {
-		sayHi("mike") ;
+	
+	// 	In this condition IF THERE IS NO STATIC IN sayHi() this function/method
+	//	cannot work
+	//	sayHi("Mike") ; 
+	//------------------------------------------------------------------------------------	
+	//	This condition can work without static in sayHi()
+		App myApp = new App () ;
+		myApp.sayHi ("Mike") ; 
+		
 	}
 	
 	public void sayHi(String name) {
-		System.out.println("Hello" + name) ;
-	}
+		System.out.println("Hello " + name) ;
+	}	
 }
