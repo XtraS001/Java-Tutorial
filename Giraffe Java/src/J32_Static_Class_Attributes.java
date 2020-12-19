f76ac05e8fde4452089622ad01e167e409924457
XtraS001 <65765051+XtraS001@users.noreply.github.com>
2020-12-19 23:33:00 +0800

    Update J32_Static_Class_Attributes.java

--- Giraffe Java/src/J32_Static_Class_Attributes.java
+++ Giraffe Java/src/J32_Static_Class_Attributes.java
@@ -1,16 +1,17 @@
 
 public class J32_Static_Class_Attributes {
 	public static void main (String [] args) {
-		Song song1 = new Song ("Song1", "artist1", 200);
-		Song song2 = new Song ("Song2", "artist2", 200);
+		Song song1 = new Song ("Song1", "artist1", 200);	//Song count = 1
 		
-		System.out.println(song1.getSongsCount()) ;
-		System.out.println(song2.getSongsCount()) ;
+		Song song2 = new Song ("Song2", "artist2", 200);	//Song count = 2
 		
-		Song song3 = new Song ("Song3", "artist3", 300);
-		System.out.println(song1.getSongsCount()) ;
-		System.out.println(song2.getSongsCount()) ;
+		System.out.println(song1.getSongsCount()) ;		//2
+		System.out.println(song2.getSongsCount()) ;		//2
 		
+		Song song3 = new Song ("Song3", "artist3", 300);	//Song count = 3
+		System.out.println(song1.getSongsCount()) ;		//3
+		System.out.println(song2.getSongsCount()) ;		//3
+
 	}
 }
 
