539218e52cd68e6b9d8581d40e17bb55876ce1f8
XtraS001 <65765051+XtraS001@users.noreply.github.com>
2020-12-19 22:20:12 +0800

    Update J13_Methods_aka_Functions.java

--- Giraffe Java/src/J13_Methods_aka_Functions.java
+++ Giraffe Java/src/J13_Methods_aka_Functions.java
@@ -1,7 +1,7 @@
 
 public class J13_Methods_aka_Functions {
-	public static void main (String [] arg) {
-		
+	public static void main (String [] arg) 
+	{	
 		sayHi() ;
 		
 		String name = "Billy" ;
@@ -9,12 +9,15 @@ public class J13_Methods_aka_Functions {
 		
 		saySmtg( name , age ) ;
 	}
-	
-	public static void sayHi() {
+
+	public static void sayHi() 
+	{
 		System.out.println("Hello Mike");
 	}
-	
-	public static void saySmtg( String name, int age ) {
+
+	public static void saySmtg( String name, int age ) 
+	{
 		System.out.println("hello " + name +" Your are " + age+ " years old.") ;
 	}
+		
 }
